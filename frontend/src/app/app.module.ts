fbbb172a6e24333eb07830aaebfd4f602ad44d0c
Richard Kille <r.kille@teluxhd.com>
2018-07-20 15:12:31 +0100

    Initial Angular creation

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule} from "@angular/material";
+import { CoreComponent } from './core/core.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoreComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
